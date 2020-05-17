7e77b768e59bd273805283223a0febbcd1b3d752
Messier77 <Messier77@users.noreply.github.com>
2020-05-17 20:22:05 +0300

    fixing project

---
+++ src/robots.js
@@ -0,0 +1,92 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Shay Given",
+        username: "Shay",
+        email: "shay@april.biz"
+    },
+    {
+        id: 2,
+        name: "Roy Keane",
+        username: "Keano",
+        email: "roy@keano.com"
+    },
+    {
+        id: 3,
+        name: "Ryan Giggs",
+        username: "Giggsy",
+        email: "ryan@manutd.com"
+    },
+    {
+        id: 4,
+        name: "Wayne Rooney",
+        username: "Wazza",
+        email: "wazza@everton.com"
+    },
+    {
+        id: 5,
+        name: "Roy M'Kaaay",
+        username: "Roy",
+        email: "roy@depor.co"
+    },
+    {
+        id: 6,
+        name: "Diego Tristan",
+        username: "Tristi",
+        email: "diego@superdepor.com"
+    },
+    {
+        id: 7,
+        name: "Paolo Maldini",
+        username: "Paolo",
+        email: "paolo@acmilan.it"
+    },
+    {
+        id: 8,
+        name: "Franco Baresi",
+        username: "Baresi",
+        email: "franco@baresi.it"
+    },
+    {
+        id: 9,
+        name: "Andres Iniesta",
+        username: "Don Andres",
+        email: "andres@fcb.es"
+    },
+    {
+        id: 10,
+        name: "Karim Benzema",
+        username: "Rashid",
+        email: "rashid@rm.es"
+    },
+    {
+        id: 11,
+        name: "Paolo Dybala",
+        username: "Paolito",
+        email: "dybala@juve.it"
+    },
+    {
+        id: 12,
+        name: "Big Lebowski",
+        username: "The Dude",
+        email: "dude@tbl.us"
+    },
+    {
+        id: 13,
+        name: "Gary Neville",
+        username: "Gazza",
+        email: "gn@manu.co.uk"
+    },
+    {
+        id: 14,
+        name: "Paul Pogba",
+        username: "Pogboom",
+        email: "pp@manu.co.uk"
+    },
+    {
+        id: 15,
+        name: "Robert Pires",
+        username: "Pireu",
+        email: "rp@ars.co.uk"
+    },
+];
